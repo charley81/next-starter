b3f154587e366c16675fbfb5c400bdafc33d0844
ChrisKnowsDev <chrisharley81@gmail.com>
2021-06-29 10:53:19 -0400

    routing section complete

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
-export default () => <h1>Index Page</h1>
+const Page = () => (
+  <div>
+    <h1>Index Page</h1>
+    <Link href="/notes">
+      <a>To Notes</a>
+    </Link>
+  </div>
+)
+
+export default Page
