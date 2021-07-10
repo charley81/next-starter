b41dd7abba87ca8a236530912d4af0e4c2a0e108
ChrisKnowsDev <chrisharley81@gmail.com>
2021-07-10 11:22:22 -0400

    covered fetching data

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx } from 'theme-ui'
+import Link from 'next/link'
+
+export default ({content}) => (
+    <div sx={{ height: `calc(100vh - 60px)` }}>
+      <div
+        sx={{
+          variant: 'containers.page',
+          display: 'flex',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <h1 sx={{ fontSize: 8, my: 0 }}>
+          {content.title}
+        </h1>
+      </div>
+    </div>
+  )
+
+  export function getStaticProps() {
+    // get data from cms
+
+    return {
+      props: {
+        content: {
+          title: 'This is my really nice app'
+        }
+      }
+    }
+  }
+
+
+
+
